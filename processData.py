f7c890e868ab19a953db2b2ae9ffd5b3d3a2a90c
aisu-programming <aisu.programming@gmail.com>
2020-10-24 23:02:01 +0800

    Add function to data processor

--- processData.py
+++ processData.py
@@ -36,7 +36,7 @@ def read_input_data():
 
     for song_index in range(file_amount):
         
-        file_name = file_directory + f'/{song_index+1}/feature.json'
+        file_name = f'{file_directory}/{song_index+1}/feature.json'
         with open(file_name) as f:
             data = json.load(f)
 
@@ -69,7 +69,7 @@ def process_answer_data():
 
         answer_data = []
 
-        file_name = file_directory + f'/{song_index+1}/ground_truth.txt'
+        file_name = f'{file_directory}/{song_index+1}/ground_truth.txt'
         with open(file_name) as f:
             while True:
                 row = f.readline()
@@ -94,16 +94,11 @@ def process_answer_data():
     return all_answer_data
 
 
-if __name__ == "__main__":
+def match_data_and_write_into_csv(all_input_data, all_answer_data, all_in_one=False):
 
-    if debug_mode: print('\nDEBUG MODE\n')
-    else: print('\nNORMAL MODE\n')
+    if all_in_one: print('All in one: ON\n')
+    else: print('All in one: OFF\n')
     
-    all_input_data = read_input_data()
-    all_answer_data = process_answer_data()
-    
-    # print('index:  input <---> answer')
-    # print('--------------------------')
     for index in range(file_amount):
         # 刪除多餘的辨識答案
         while len(all_answer_data[index]) > len(all_input_data[index]):
@@ -112,64 +107,79 @@ if __name__ == "__main__":
         while len(all_answer_data[index]) < len(all_input_data[index]):
             all_answer_data[index].append('N')
 
+    columns = [
+        'Song No.',
+        'Frame No.',
+        'chroma_cqt C',
+        'chroma_cqt C#',
+        'chroma_cqt D',
+        'chroma_cqt D#',
+        'chroma_cqt E',
+        'chroma_cqt F',
+        'chroma_cqt F#',
+        'chroma_cqt G',
+        'chroma_cqt G#',
+        'chroma_cqt A',
+        'chroma_cqt A#',
+        'chroma_cqt B',
+        'chroma_cens C',
+        'chroma_cens C#',
+        'chroma_cens D',
+        'chroma_cens D#',
+        'chroma_cens E',
+        'chroma_cens F',
+        'chroma_cens F#',
+        'chroma_cens G',
+        'chroma_cens G#',
+        'chroma_cens A',
+        'chroma_cens A#',
+        'chroma_cens B',
+        'label'
+    ]
+
     toolbar_width = 100
-    sys.stdout.write("Combining inputs and answers.\n[%s]" % (" " * toolbar_width))
+    if all_in_one: sys.stdout.write("Combining all inputs and answers together.\n[%s]" % (" " * toolbar_width))
+    else: sys.stdout.write("Combining inputs and answers in each files.\n[%s]" % (" " * toolbar_width))
     sys.stdout.flush()
     sys.stdout.write("\b" * (toolbar_width+1))
 
     all_data = np.empty((0, 27))
-    for index in range(file_amount):
+    for song_index in range(file_amount):
 
-        frame_length = len(all_input_data[index])
-        song_index_array = np.array([[index+1] * frame_length]).transpose()
+        frame_length = len(all_input_data[song_index])
+        song_index_array = np.array([[song_index+1] * frame_length]).transpose()
         frame_index_array = np.array([range(1, frame_length+1)]).transpose()
-        input_data = np.array(all_input_data[index])
-        answer_data = np.array([all_answer_data[index]]).transpose()
-
+        input_data = np.array(all_input_data[song_index])
+        answer_data = np.array([all_answer_data[song_index]]).transpose()
         combined_data = np.hstack((song_index_array, frame_index_array, input_data, answer_data))
-        all_data = np.vstack((all_data, combined_data))
+
+        if all_in_one: all_data = np.vstack((all_data, combined_data))
+        else:
+            data = pd.DataFrame(combined_data, columns=columns)
+            data.to_csv(f'{file_directory}/{song_index+1}/data.csv')
 
         if debug_mode:
             sys.stdout.write("=" * 5)
             sys.stdout.flush()
-        elif (index + 1) % (file_amount / toolbar_width) == 0:
+        elif (song_index + 1) % (file_amount / toolbar_width) == 0:
             sys.stdout.write("=")
             sys.stdout.flush()
     
     sys.stdout.write("]\n\n")
-    print('Convert into DataFrame... ', end='')
-    data = pd.DataFrame(
-        all_data,
-        columns=[
-            'Song No.',
-            'Frame No.',
-            'chroma_cqt C',
-            'chroma_cqt C#',
-            'chroma_cqt D',
-            'chroma_cqt D#',
-            'chroma_cqt E',
-            'chroma_cqt F',
-            'chroma_cqt F#',
-            'chroma_cqt G',
-            'chroma_cqt G#',
-            'chroma_cqt A',
-            'chroma_cqt A#',
-            'chroma_cqt B',
-            'chroma_cens C',
-            'chroma_cens C#',
-            'chroma_cens D',
-            'chroma_cens D#',
-            'chroma_cens E',
-            'chroma_cens F',
-            'chroma_cens F#',
-            'chroma_cens G',
-            'chroma_cens G#',
-            'chroma_cens A',
-            'chroma_cens A#',
-            'chroma_cens B',
-            'label'
-        ]
-    )
-    print(f"Done.\n\nWriting data into '{output_file_name}'... ", end='')
-    data.to_csv(output_file_name)
+
+    if all_in_one:
+        print('Convert into DataFrame... ', end='')
+        data = pd.DataFrame(all_data, columns=columns)
+        print(f"Done.\n\nWriting data into '{output_file_name}'... ", end='')
+        data.to_csv(output_file_name)
     print('Done.')
+
+
+if __name__ == "__main__":
+
+    if debug_mode: print('\nDEBUG MODE\n')
+    else: print('\nNORMAL MODE\n')
+
+    all_input_data = read_input_data()
+    all_answer_data = process_answer_data()
+    match_data_and_write_into_csv(all_input_data, all_answer_data, all_in_one=False)
